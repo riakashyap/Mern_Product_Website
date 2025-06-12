967441b4ddfb182256cee9875c7845a13f8c802d
riakashyap <ria.k.kashyap@gmail.com>
2025-06-12 16:55:40 +0530

    Route Issue Resolved

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import productRoutes from "./routes/product.js";
 import { mongo } from 'mongoose';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in req.body
 
 app.use("/api/products",productRoutes)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log('Server is running on port 5000');
 });
